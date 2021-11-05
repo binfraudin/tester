023c77ca829e27977c4fb823f67e2822e373aedd
binfraudin <servvvone@gmail.com>
2021-11-05 17:06:33 +0800

    first commit

---
+++ bots/discord/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiurl: 'http://157.230.233.113:1337',
+    apipassword: '5e320093e019bf3ffe3e9d1b73a64caac1737907d70f8eba19add2f6c4633006',
+
+    discordtoken: 'OTA2MDMwOTc3Mzk3MTkwNjU4.YYStGg.KGtAi9JrZ73f0SC8lE1dpm8P4Lw',
+    discordprefix: '!',
+    secretpassword: '2020blkout6080',
+    botuser_rolename: 'Bot User',
+    admin_rolename: 'Admin'
+};
